44ad6acaac92d2731f62d6be500a24f6b4b1c3bb
ryankav <ryan.p.kav@gmail.com>
2022-02-25 22:21:31 +0000

    Titles for the print

--- lecture_1/instrumented.h
+++ lecture_1/instrumented.h
@@ -7,6 +7,7 @@ struct instrumented {
   size_t val;
   static const size_t number_ops = 7;
   static const T* count[7];
+  static const char* counter_names[7] = { "n", "copy", "assign", "destruct", "default", "equal", "less" };
 
   instrumented(): val() {}
 };
