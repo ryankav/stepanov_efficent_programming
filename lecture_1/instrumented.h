4083ef547160aee2ff79e0ca65ac83dccde78bf8
ryankav <ryan.p.kav@gmail.com>
2022-02-27 23:40:15 +0000

    Small changes and setting up semi-regular type

--- lecture_1/instrumented.h
+++ lecture_1/instrumented.h
@@ -2,17 +2,48 @@
 #define INTSTRUMENTED_H
 
 template <typename T>
-struct instrumented {
+struct instrumented_meta {
+  const T count[7];
+  const char* headers[7];
 
-  size_t val;
-  static const size_t number_ops;
-  static const T count[7];
-  static const char* counter_names[7];
-
-  instrumented(): val() {}
+  instrumented_meta() : headers{ "n", "copy", "assign", "destruct", "default", "equal", "less" } {}
+  ~instrumented_meta() {}
+  instrumented_meta(const instrumented_meta& x): count(x.count), headers(x.headers) {}
+  instrumented_meta& operator=(const instrumented_meta& x) {
+    count = x.count;
+    headers = x.headers;
+    return *this;
+  }
+  increment(int index) {
+    ++count[index];
+  }
 };
 
 template <typename T>
-const T instrumented<T>::count[] = {};
+struct instrumented {
+
+  size_t val;
+  static const instrumented_meta<T> meta_;
+  static const size_t number_ops;
+  static const 
+
+  instrumented(const instrumented& x) : val(x.value) {
+    meta_.increment(1);
+  }
+
+  instrumented& operator=(const instrumented& x) {
+    val = x.value;
+    meta_.increment(2);
+    return *this;
+  }
+
+  ~instrumented() {
+    meta_.increment(3);
+  }
+
+  instrumented() {
+    meta_.increment(4);
+  }
+};
 
 #endif
